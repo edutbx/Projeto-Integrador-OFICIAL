f828fc4a0cc2e49d98c9cfe3868cae8820107b1a
pedro-albertini <pedro.albertini100@gmail.com>
2025-05-06 20:35:29 -0300

    gerente responsivo

--- paciente/menuHamburguer.js
+++
@@ -1,6 +0,0 @@
-const hamburguer = document.querySelector(".hamburguer");   
-const navList = document.querySelector(".nav");
-
-hamburguer.addEventListener("click", () => {
-    navList.classList.toggle("active");
-});
