02f6fa6c04970582dbb7ec4cf082a2f42c980fba
MSCiriaco <matheus.ciriaco@fatec.sp.gov.br>
2025-09-17 15:30:40 -0300

    Projeto Java

---
+++ src/main/java/com/br/iasaude/saudemais/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.br.iasaude.saudemais.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/", "/index", "/index.html", "/CSS/**", "/img/**", "/styleHome/**", "/styleLogin/**", "/styleMedico/**", "/styleGestor/**", "/static/**", "/**.css", "/**.js", "/**.png", "/**.jpg", "/**.jpeg", "/**.ico", "/login.html").permitAll()
+                .anyRequest().permitAll()
+            );
+        return http.build();
+    }
+}
